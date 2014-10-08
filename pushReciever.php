0238306c989f13f789dcae1e7bcad021cebb4760
sureshprasanna70 <sureshprasanna70@gmail.com>
2014-10-08 10:48:56 +0530

    Removed comments

--- pushReciever.php
+++ pushReciever.php
@@ -3,17 +3,13 @@
 $repo_dir = '/home/<username>/<repo-name>.git';
 $web_root_dir = '/home/<username>/www';
 
-// Full path to git binary is required if git is not in your PHP user's path. Otherwise just use 'git'.
 $git_bin_path = 'git';
 
 $update = false;
 
-// Parse data from Bitbucket hook payload
 $payload = json_decode($_POST['payload']);
 
 if (empty($payload->commits)){
-  // When merging and pushing to bitbucket, the commits array will be empty.
-  // In this case there is no way to know what branch was pushed to, so we will do an update.
   $update = true;
 } else {
   foreach ($payload->commits as $commit) {
