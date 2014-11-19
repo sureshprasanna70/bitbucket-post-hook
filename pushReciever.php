5908bc182afb9c4e7400152ddd2240a816de3e77
sureshprasanna70 <sureshprasanna70@gmail.com>
2014-11-19 21:00:14 +0530

    Removed var dump

--- pushReciever.php
+++ pushReciever.php
@@ -5,7 +5,7 @@ $web_root_dir = '/home/<username>/www';
 $git_bin_path = 'git';
 $update = false;
 $payload = urldecode(file_get_contents('php://input'));
-error_log(var_dump(json_decode($payload,true)));
+error_log(json_decode($payload,true));
 if (empty($payload->commits)){
   $update = true;
 } else {
