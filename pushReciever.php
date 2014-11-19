9580cc876ad660f89cc69ba913e8ce3df0c01a21
sureshprasanna70 <sureshprasanna70@gmail.com>
2014-11-19 21:09:03 +0530

    testing decoding

--- pushReciever.php
+++ pushReciever.php
@@ -5,7 +5,8 @@ $web_root_dir = '/home/<username>/www';
 $git_bin_path = 'git';
 $update = false;
 $payload = urldecode(file_get_contents('php://input'));
-error_log(substr($payload,8,strlen($payload)));
+$tobedecoded=substr($payload,8,strlen($payload));
+error_log(var_dump(json_decode($tobedecoded,1)));
 if (empty($payload->commits)){
   $update = true;
 } else {
