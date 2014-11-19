146799670f978d75f19f528ec3237baf96802c55
sureshprasanna70 <sureshprasanna70@gmail.com>
2014-11-19 20:30:21 +0530

    Error log

--- pushReciever.php
+++ pushReciever.php
@@ -5,6 +5,7 @@ $web_root_dir = '/home/<username>/www';
 $git_bin_path = 'git';
 $update = false;
 $payload = file_get_contents('php://input');
+error_log($payload);
 if (empty($payload->commits)){
   $update = true;
 } else {
