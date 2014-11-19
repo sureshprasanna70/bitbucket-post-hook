5bad55978825f6f517b9089656d772a9579e849d
sureshprasanna70 <sureshprasanna70@gmail.com>
2014-11-19 21:48:51 +0530

    Direct access worked staying with that

--- pushReciever.php
+++ pushReciever.php
@@ -4,8 +4,5 @@ $repo_dir = '/home/<username>/<repo-name>.git';
 $web_root_dir = '/home/<username>/www';
 $git_bin_path = 'git';
 $update = false;
-$payload = urldecode(file_get_contents('php://input'));
-$tobedecoded=substr($payload,8,strlen($payload));
-$decoded=json_decode($tobedecoded);
 error_log($_POST["payload"]);
 ?>
