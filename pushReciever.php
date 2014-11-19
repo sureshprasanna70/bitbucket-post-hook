9c77234e99cb76a6636f2cd83d7c3abda3fb50b6
sureshprasanna70 <sureshprasanna70@gmail.com>
2014-11-19 21:10:28 +0530

    Only json decode

--- pushReciever.php
+++ pushReciever.php
@@ -6,7 +6,7 @@ $git_bin_path = 'git';
 $update = false;
 $payload = urldecode(file_get_contents('php://input'));
 $tobedecoded=substr($payload,8,strlen($payload));
-error_log(var_dump(json_decode($tobedecoded,1)));
+error_log(json_decode($tobedecoded));
 if (empty($payload->commits)){
   $update = true;
 } else {
