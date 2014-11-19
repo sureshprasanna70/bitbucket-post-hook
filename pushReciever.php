fea83779038b6ba28727aa382442b31483c8743e
sureshprasanna70 <sureshprasanna70@gmail.com>
2014-11-19 20:49:41 +0530

    Url decode

--- pushReciever.php
+++ pushReciever.php
@@ -4,7 +4,7 @@ $repo_dir = '/home/<username>/<repo-name>.git';
 $web_root_dir = '/home/<username>/www';
 $git_bin_path = 'git';
 $update = false;
-$payload = file_get_contents('php://input');
+$payload = url_decode(file_get_contents('php://input'));
 error_log($payload);
 if (empty($payload->commits)){
   $update = true;
