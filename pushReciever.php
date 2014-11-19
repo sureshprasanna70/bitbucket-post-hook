7ea07bb8d12b11be6f7978e48b363535453963f3
sureshprasanna70 <sureshprasanna70@gmail.com>
2014-11-19 20:26:08 +0530

    Using file read

--- pushReciever.php
+++ pushReciever.php
@@ -1,9 +1,10 @@
 <?php
+
 $repo_dir = '/home/<username>/<repo-name>.git';
 $web_root_dir = '/home/<username>/www';
 $git_bin_path = 'git';
 $update = false;
-$payload = json_decode($_POST['payload']);
+$payload = file_get_contents('php://input');
 if (empty($payload->commits)){
   $update = true;
 } else {
